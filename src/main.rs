03de9306426fd1f0f9311909025d6bbb8cf48786
Manan Karnik <manankarnik03@gmail.com>
2022-09-25 15:24:39 +0530

    Basic typing test and mistake tracking

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,57 @@ use ncurses::*;
 fn main() {
     // Start ncurses
     initscr();
+    noecho();
+    curs_set(CURSOR_VISIBILITY::CURSOR_INVISIBLE);
     
+    start_color();
+    init_pair(1, COLOR_WHITE, COLOR_BLACK);
+    init_pair(2, COLOR_GREEN, COLOR_BLACK);
+    init_pair(3, COLOR_RED, COLOR_BLACK);
+
+
+    let mut quit = false;
+    let mut text = "Hello World";
+    let mut idx = 0;
+    let mut mistakes = 0;
+
     // Print to buffer
-    addstr("Hello, world!");
+    attron(COLOR_PAIR(1));
+    addstr(text);
+    mv(2, 0);
+    addstr("Press '~' to exit");
+    mv(0, 0);
 
-    // Update screen
-    refresh();
+    while !quit {
+        let key = getch();
 
-    // Wait for keypress
-    getch();
-
+        if idx < text.len() {
+            let mut ch = text.chars().nth(idx).unwrap();
+            // Wait for keypress
+            if key as u8 as char == ch {
+                attron(COLOR_PAIR(2));
+                addch(ch as u32);
+                mv(0, (idx + 1).try_into().unwrap());
+                idx += 1;
+            } else {
+                attron(COLOR_PAIR(3));
+                addch(ch as u32);
+                mv(0, (idx).try_into().unwrap());
+                mistakes += 1;
+            }
+            if idx == text.len() {
+                mvprintw(1, 0, "Completed with ");
+                printw(&mistakes.to_string().to_owned());
+                printw(" mistakes");
+            }
+        }
+        if key as u8 as char == '~' {
+            quit = true;
+        }
+        // Update screen
+        refresh();
+    }
     // Terminate ncurses
     endwin();
 }
+
